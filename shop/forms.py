55a77841fd0dfd1fdeb3b78e07b9b8560ee8b1c0
rorlol <adjanusakov@gmail.com>
2025-06-15 13:36:37 +0600

    Add files via upload

--- shop/forms.py
+++ shop/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 
+#class SearchForm(forms.Form):
+ #   query = forms.CharField(
+  #      label=False,
+   #     required=False,
+    #    widget=forms.TextInput(attrs={'placeholder':'Поиск товаров . . .',})
+     #)
+
+
 class SearchForm(forms.Form):
+    # Одно поле для ввода поискового запроса.
     query = forms.CharField(
-        label=False,
-        required=False,
-        widget=forms.TextInput(attrs={'placeholder':'Поиск товаров . . .',})
+        label=False, # Не отображать стандартную метку поля (используем placeholder)
+        required=False, # Поиск может быть пустым (тогда отобразятся все товары)
+        widget=forms.TextInput(attrs={'placeholder': 'Поиск товаров...'})
     )
--- shop/models.py
+++ shop/models.py
@@ -18,6 +18,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, )
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name='Изображение')
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
@@ -26,3 +27,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
